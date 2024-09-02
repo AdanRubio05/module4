5c0d1aeaa2ee7a75758ebe3fb98b08823777c062
AdanRubio05 <111942322+AdanRubio05@users.noreply.github.com>
2024-09-02 16:32:29 -0500

    parseInt used

--- appz.js
+++ appz.js
@@ -9,3 +9,12 @@ let fruit = "Apple" ;
 let letter = fruit.charAt(4) ;
 
 document.getElementById("answer1").innerHTML = letter ;
+
+document.getElementById("order").innerHTML =
+    parseInt("-3") + " ," +
+    parseInt("-2.46") + " ," +
+    parseInt("-1.00875") + " ," +
+    parseInt("0") + " ," +
+    parseInt("1") + " ," +
+    parseInt("2.0467") + " ," +
+    parseInt("3.0") + " !" ;
