9a197ef4efab0d3703bf3c008aeee47f10932de1
AdanRubio05 <111942322+AdanRubio05@users.noreply.github.com>
2024-09-02 16:21:43 -0500

    letter did display
    changed to e for readability and better answer

--- appz.js
+++ appz.js
@@ -6,6 +6,6 @@ let fullName = firstName + " " + lastName ;
 document.getElementById("string").innerHTML = "I, " + fullName + " created this sentence in JS." ;
 
 let fruit = "Apple" ;
-let letter = fruit.charAt(3) ;
+let letter = fruit.charAt(4) ;
 
 document.getElementById("answer1").innerHTML = letter ;
